8da0094d0e016721891f8d960846b0d4bebbcf03
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-02-21 16:41:53 +0000

    trying to render a signle card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+//this is importing the portfolio container which the page is generated from 
 import PortfolioContainer from "./components/PortfolioContainer";
 
-
+//everything starts from this one root element 
 const App = () => <PortfolioContainer />;
 
 export default App;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import renderCard from '../card';
 //import styles from '../../styles/'
 
 export default function Blog() {
@@ -10,7 +11,7 @@ export default function Blog() {
       </p>
 
         <div className='card'>
-          
+          <renderCard/>
         </div>
     </div>
   );
