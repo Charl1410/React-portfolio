e45909774a04740f11667fb38a610cd49087da26
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-02-21 17:51:25 +0000

    got link working to repos from each card

--- src/components/renderCard.js
+++ src/components/renderCard.js
@@ -6,14 +6,29 @@ const createCard = () => {
   const cardInfo = [
     {
       image: "https://www.freecodecamp.org/news/content/images/2021/04/BG.png",
+      gitHub: "https://github.com/Charl1410/coding-blog",
       title: "MVC tech blog",
       text: "Full stack blog made with SQL express and handlebars.js",
     },
     {
       image: "https://enviame.io/wp-content/uploads/2022/08/ecommerce-chile-2022.jpg",
+      gitHub: "https://github.com/Charl1410/e-commerce-back-end",
       title: "e-commerce back end",
-      text: "bac end application using express to create routes in an e-commerce product tracker ",
-    }
+      text: "back end application using express to create routes in an e-commerce product tracker ",
+    },
+    {
+      image: "https://www.pageuppeople.com/wp-content/uploads/2019/01/Top-60-Employee-Engagement-image43-1200x720.png",
+      gitHub: "https://github.com/Charl1410/Employee-tracker",
+      title: "Employee tracker",
+      text: "Back end application using inquirer and MySQL to track employees in departments",
+    },
+    {
+      image: "https://clickup.com/blog/wp-content/uploads/2020/01/note-taking.png",
+      gitHub: "https://github.com/Charl1410/note-taking-application",
+      title: "Note taking application",
+      text: "back end application using express to create routes in an e-commerce product tracker ",
+    },
+
 
   ]
 
@@ -27,7 +42,7 @@ const createCard = () => {
         <Card.Text>
          {card.text}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button href={card.gitHub} className="card-button" variant="primary">Visit Repo</Button>
       </Card.Body>
     </Card>
   );
