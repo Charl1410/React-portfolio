d0d9ab06a197597ca5541545be0ab2c58dc845e5
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-02-21 17:21:21 +0000

    added data to get multiple cards rendering

--- src/components/renderCard.js
+++ src/components/renderCard.js
@@ -7,6 +7,11 @@ const createCard = () => {
       image: "https://www.freecodecamp.org/news/content/images/2021/04/BG.png",
       title: "MVC tech blog",
       text: "Full stack blog made with SQL express and handlebars.js",
+    },
+    {
+      image: "https://enviame.io/wp-content/uploads/2022/08/ecommerce-chile-2022.jpg",
+      title: "e-commerce back end",
+      text: "bac end application using express to create routes in an e-commerce product tracker ",
     }
 
   ]
