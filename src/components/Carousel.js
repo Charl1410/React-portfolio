4dd510511a0fb46b6802da605ef3030a71985892
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-02-22 19:45:05 +0000

    formatting of image and text first slide of carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,21 +1,23 @@
 import Carousel from 'react-bootstrap/Carousel';
-import Memoji from './Memoji'
 
 function CreateCarousel() {
   return (
     <Carousel className="Carousel">
-      <Carousel.Item>
+      <Carousel.Item  >
+        <div className="center">
         <img
-          className="d-block w-100"
+          className="emoji"
           src="/assets/images/photo.PNG"
           alt="First slide"
         />
-        <Memoji />
-        <Carousel.Caption>
-          <h3 className="carousel-font">Hey i'm Charlotte</h3>
+        </div>
+        <div class="carousel-text">
+        <h3 className="carousel-font">Hey i'm Charlotte</h3>
           <p className="carousel-font">I am currently studying web development at the University of Birmingham!</p>
-        </Carousel.Caption>
+        </div>
       </Carousel.Item>
+
+
       <Carousel.Item>
         <img
           className="d-block w-100"
@@ -28,6 +30,8 @@ function CreateCarousel() {
           <p className="carousel-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
+
+
       <Carousel.Item>
         <img
           className="d-block w-100"
--- src/components/Memoji.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Memoji() {
-  return (
-    <img src="../../public/photo.PNG" alt="memoji" />
-  );
-}
