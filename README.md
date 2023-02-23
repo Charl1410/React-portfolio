e9b3072cd96df48a4941ad728431a80da24ed0ed
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-02-23 16:50:29 +0000

    readme and screenshots added

--- README.md
+++ README.md
@@ -14,19 +14,27 @@
 This application is built using react and bootstrap to display information about me, my previous coding projects and my resume. 
 
 ## Initialisation
-To use this application node.js must be installed: https://nodejs.org/en/download/ <br />
 
 **The packages required are:**
- * **package name**  @5.0.0
- 
 
-All dependencies should alreadt be in the package.json file and installed through running ```npm i``` in the terminal.
+ * **package name**  @5.0.0
+* **react**  @17.0.2
+* **react-bootstrap**  @2.7.2
+* **react-dom**  @17.0.2
+* **react-scripts**  @5.0.1
+* **web-vitals**  @2.1.4
+
+
+All dependencies should alreadt be in the package.json file and installed through running `npm i` in the terminal.
 
 ## Usage
-
+To run this application locally all files and dependencies must be downloaded. Firstly run `npm run build` and finally `npm run start` and visit `localhost:3000` in your browser. 
 
 ## Screenshots 
-
+![]()
+![]()
+![]()
+![]()
 
 ## Contributors
 
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -8,28 +8,21 @@ function NavTabs({ currentPage, handlePageChange }) {
     <>
       <Navbar className="navbar" bg="primary" variant="dark">
         <Container>
-        <Navbar.Brand style={{fontSize:'3rem'}}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Charlotte Griffin</Navbar.Brand>
+        <Navbar.Brand className="my-name">Charlotte Griffin</Navbar.Brand>
           <Navbar.Brand href="#home" 
           onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+         
           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>About me</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#portfolio"
           onClick={() => handlePageChange('Portfolio')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Nav.Link>
             <Nav.Link href="#contact"
           onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >Contact me</Nav.Link>
             <Nav.Link href="#resume"
           onClick={() => handlePageChange('Resume')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</Nav.Link>
           </Nav>
         </Container>
