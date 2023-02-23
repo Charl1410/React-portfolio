3db405012e6f402a1a7e4ca4b42a9c905a81e95f
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-02-23 23:32:16 +0000

    Updates

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-//this is importing the portfolio container which the page is generated from 
-import PortfolioContainer from "./components/PortfolioContainer";
-
-//everything starts from this one root element 
-const App = () => <PortfolioContainer />;
-
-export default App;
