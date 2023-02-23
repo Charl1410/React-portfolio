35e8532f30a5cfde0db896bdb9b9e192c19ab053
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-02-23 15:41:27 +0000

    refractoring css

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -30,29 +30,32 @@ function CreateCarousel() {
           </div>
 
             <div class="carousel-text">
-            <h3 className="carousel-font">Here are some coding languages I am learning</h3>
-              <p className="carousel-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3 className="carousel-font">Here are some coding languages and dependencies I have been using</h3>
+              <p className="carousel-font"></p>
             </div>
       </Carousel.Item>
 
 
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://assets-global.website-files.com/5e39e095596498a8b9624af1/5f6e93d250a6d04f4eae9f02_Backgrounds-WFU-thumbnail-(size).jpg"
-          alt="Third slide"
-        />
+          <div className="center">
 
-        <Carousel.Caption>
-          <h3 className="carousel-font">More about me</h3>
-          <p className="carousel-font">
-            Before becoming interested in coding I got my degree in psychology from the University of Sussex. In my free time I 
-            love to go bouldering, going to the gym and going out for coffee! As a create persion I am very passionate about UI therefore, I am really enjoying using react.
-            To see some of the projects I have donr throughout this course check our my porfolio page.
-            
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://assets-global.website-files.com/5e39e095596498a8b9624af1/5f6e93d250a6d04f4eae9f02_Backgrounds-WFU-thumbnail-(size).jpg"
+              alt="Third slide"
+            />
+          </div>
+
+            <Carousel.Caption className="card-3">
+              <h3 className="carousel-font">More about me</h3>
+              <p className="carousel-font">
+                Before becoming interested in coding I got my degree in psychology from the University of Sussex. In my free time I 
+                love to go bouldering, going to the gym and going out for coffee! As a create persion I am very passionate about UI therefore, I am really enjoying using react.
+                To see some of the projects I have donr throughout this course check our my porfolio page.
+                
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
     </Carousel>
   );
 }
